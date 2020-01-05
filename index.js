ee258c4201660d0ab3ad7e90682aac3f179f0bd5
Diego Massanti <diego@massanti.com>
2020-01-05 15:18:47 -0300

    Fixes

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const Api = require('./api');
 module.exports = class UCRM {
   constructor(config = {}) {
     this.api = new Api(config);
-    console.log(`FD UCRM Connector instance up and ready for ${config.fqdn}`);
+    console.log(`UCRM Connector instance up and ready for ${config.fqdn}`);
   }
 };
